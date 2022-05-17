6307e6c27c028d6293b92528081c43e2e9176c6f
Emmanuel Accomford <eaccomford@gmail.com>
2022-05-17 08:24:15 +0000

    update

--- buy-product.php
+++ buy-product.php
@@ -6,70 +6,85 @@ include('includes/head.php')
 
 
 
+<div style="height: 90vh; margin-bottom: 5rem">
+    <section class="tm-margin-large-top" id="elementID">
+        <div class="uk-container">
 
-<section class="tm-margin-large-top">
-    <div class="uk-container">
+            <h2 class="uk-margin-medium-bottom xuk-heading-hero" style="margin-top: -1em">Fill the form for:
+                <?= $_GET['name'] ?></h2>
 
-        <h2 class="uk-margin-medium-bottom xuk-heading-hero" style="margin-top: -1em">Fill the form for:
-            <?= $_GET['name'] ?></h2>
+            <hr class="uk-margin-remove">
+            <div id="responseMsg" class="uk-margin-medium-bottom xuk-heading-hero" style="margin-top: .4em"></div>
 
-        <hr class="uk-margin-remove">
-        <div id="responseMsg" class="uk-margin-medium-bottom xuk-heading-hero" style="margin-top: .4em"></div>
-        <div style="margin-top: -4em">
-            <form id="save_productfrm"
-                class="uk-form-stacked uk-margin-xlarge-top uk-margin-xlarge-bottom tm-form-contact">
-                <div class="uk-margin-large-bottom">
-                    <label class="uk-form-label uk-margin-small-bottom" for="form-stacked-text">Customer Name</label>
-                    <div class="uk-form-controls">
-                        <input class="uk-input uk-form-large tm-form-xlarge uk-border-rounded" id="customer_name"
-                            id="form-name" type="text" placeholder="Enter your name">
-                        <input type="hidden" id="product_id" value="<?= $_GET['id']; ?>">
+            <div class="uk-grid-medium uk-child-width-1-3@m uk-grid-small uk-grid-match tm-margin-large-top uk-text-center"
+                data-uk-grid data-uk-scrollspy="cls: uk-animation-slide-bottom-medium; delay: 80"
+                style=" display: flex; justify-content:center">
+                <div style="text-align:center;display: none; margin-bottom: 3rem" id="mainmenu">
+                    <a href="index" class="uk-card uk-card-box uk-card-body uk-box-shadow-medium uk-border-rounded">
+                        <span class="uk-icon-forder uk-border-rounded"><span
+                                data-uk-icon="icon: home; ratio: 2"></span></span>
+                        <p>Return to Main Mainu</p>
+                    </a>
+                </div>
+            </div>
+
+
+            <div style="margin-top: -8em" id="formdiv">
+                <form id="save_productfrm"
+                    class="uk-form-stacked uk-margin-xlarge-top uk-margin-xlarge-bottom tm-form-contact">
+                    <div class="uk-margin-large-bottom">
+                        <label class="uk-form-label uk-margin-small-bottom" for="form-stacked-text">Customer
+                            Name</label>
+                        <div class="uk-form-controls">
+                            <input class="uk-input uk-form-large tm-form-xlarge uk-border-rounded" id="customer_name"
+                                id="form-name" type="text" placeholder="Enter your name">
+                            <input type="hidden" id="product_id" value="<?= $_GET['id']; ?>">
+                        </div>
+                        <div class="customer_name"></div>
                     </div>
-                    <div class="customer_name"></div>
-                </div>
-                <div class="uk-margin-large-bottom">
-                    <label class="uk-form-label uk-margin-small-bottom" for="form-stacked-text">Mobile Number</label>
-                    <div class="uk-form-controls">
-                        <input id="mobile" class="uk-input uk-form-large tm-form-xlarge uk-border-rounded"
-                            id="form-email" type="number" placeholder="Enter Mobile umber">
+                    <div class="uk-margin-large-bottom">
+                        <label class="uk-form-label uk-margin-small-bottom" for="form-stacked-text">Mobile
+                            Number</label>
+                        <div class="uk-form-controls">
+                            <input id="mobile" class="uk-input uk-form-large tm-form-xlarge uk-border-rounded"
+                                id="form-email" type="number" placeholder="Enter Mobile umber">
+                        </div>
+                        <div class="mobile"></div>
                     </div>
-                    <div class="mobile"></div>
-                </div>
-                <div class="uk-margin-large-bottom">
-                    <label class="uk-form-label uk-margin-small-bottom" for="form-stacked-text">Email</label>
-                    <div class="uk-form-controls">
-                        <input id="email" class="uk-input uk-form-large tm-form-xlarge uk-border-rounded"
-                            id="form-email" type="email" placeholder="Enter your email">
+                    <div class="uk-margin-large-bottom">
+                        <label class="uk-form-label uk-margin-small-bottom" for="form-stacked-text">Email</label>
+                        <div class="uk-form-controls">
+                            <input id="email" class="uk-input uk-form-large tm-form-xlarge uk-border-rounded"
+                                id="form-email" type="email" placeholder="Enter your email">
+                        </div>
+                        <div class="email"></div>
                     </div>
-                    <div class="email"></div>
-                </div>
-                <div class="uk-margin-large-bottom">
-                    <label class="uk-form-label uk-margin-small-bottom" for="form-stacked-text">Notification
-                        Type</label>
-                    <div class="uk-form-controls">
-                        <input value="SMS" id="typesms" type="radio" checked name="notification_type"
-                            style="transform: scale(2); "> SMS
-                        <input value="EMAIL" id="typeemail" type="radio" name="notification_type"
-                            style="transform: scale(2); "> Email
+                    <div class="uk-margin-large-bottom">
+                        <label class="uk-form-label uk-margin-small-bottom" for="form-stacked-text">Notification
+                            Type</label>
+                        <div class="uk-form-controls">
+                            <input value="SMS" id="typesms" type="radio" checked name="notification_type"
+                                style="transform: scale(2); "> SMS
+                            <input value="EMAIL" id="typeemail" type="radio" name="notification_type"
+                                style="transform: scale(2); "> Email
+                        </div>
                     </div>
-                </div>
-                <div class="uk-text-left">
-                    <button class="tm-button tm-button-xlarge" id="save_product" name="send">Submit</button>
-                </div>
-                <div id="loading"
-                    style="display: none; background: white; padding: 1em; color: green; font-size: 21px; font-weight: bold; text-align:center">
-                    Processing Request...</div>
-            </form>
+                    <div class="uk-text-left">
+                        <button class="tm-button tm-button-xlarge" id="save_product" name="send">Submit</button>
+                    </div>
+                    <div id="loading"
+                        style="display: none; background: white; padding: 1em; color: green; font-size: 21px; font-weight: bold; text-align:center">
+                        Processing Request...</div>
+                </form>
+
+
+            </div>
 
 
         </div>
+    </section>
 
-
-    </div>
-</section>
-
-
-
+</div>
 
 <footer class="uk-section tm-primary-color-dark uk-light uk-text-center">
     <div class="uk-container">
@@ -177,6 +192,14 @@ $(document).ready(function() {
                     }
                     $('#loading').hide('slow');
                     $('#save_product').show('slow')
+                    $('#mainmenu').show('slow')
+                    $('#formdiv').hide('slow')
+                    // window.scrollTo(xCoord, yCoord);
+                    // $(window).scrollTop(0);
+                    $('html, body').animate({
+                        scrollTop: $("#elementID").offset().top
+                    }, 2000);
+
                 }
 
             },
--- includes/head.php
+++ includes/head.php
@@ -5,8 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Kiosk</title>
-    <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,800%7CWork+Sans:200,300%7CJosefin+Sans:100"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,800%7CWork+Sans:200,300%7CJosefin+Sans:100" rel="stylesheet">
     <link rel="stylesheet" href="css/main.css" />
     <script src="js/uikit.min.js" defer></script>
     <script src="js/uikit-icons.min.js" defer></script>
@@ -21,9 +20,11 @@
             <div class="uk-container">
                 <div data-uk-navbar>
                     <div class="uk-navbar-left">
-                        <a class="uk-navbar-item uk-logo uk-text-small" href="index">Old Mutual</a>
+                        <a class="uk-navbar-item uk-logo uk-text-small" href="index">
+                            <img src="assets/icon.png" style="width:150px; height:150px;" />
+                        </a>
                     </div>
-                    <div class="uk-navbar-center uk-visible@m">
+                    <div class=" uk-navbar-center uk-visible@m">
 
                         <ul class="uk-navbar-nav">
                             <!-- <li class="uk-active"><a href="index.html">Support</a></li>
@@ -37,7 +38,7 @@
                     <div class="uk-navbar-right">
                         <!-- <a class="uk-navbar-toggle " href="#search" data-uk-search-icon data-uk-toggle></a>
                         <a class="uk-navbar-toggle uk-hidden@m" href="#offcanvas" data-uk-navbar-toggle-icon data-uk-toggle></a> -->
-                        <a href="#">Contact</a>
+                        <a href="#">Need Help?</a>
                     </div>
                 </div>
             </div>
