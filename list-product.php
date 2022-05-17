0f6c40abf4da04db8d8c621fdc76ff4a80939c9d
Emmanuel Accomford <eaccomford@gmail.com>
2022-05-17 14:11:57 +0000

    update

--- list-product.php
+++ list-product.php
@@ -1,7 +1,11 @@
 <?php include('includes/head.php') ?>
+<style>
+.tm-margin-large-top {
+    background-color: #2196F3;
+}
+</style>
 
-<?php
-$curl = curl_init();
+<?php $curl = curl_init();
 // https://insurance.stakcloud.com/api/dms/check-status?policy_no=5001OMEP000002
 curl_setopt_array($curl, array(
     CURLOPT_URL => 'https://insurance.stakcloud.com/api/dms/get-products-selfservice?user_id=1',
@@ -27,83 +31,28 @@ $data = json_decode($response);
 
 // print_r($data->products);
 ?>
-<div style="height: 90vh; margin-bottom: 5rem">
-    <section class="tm-margin-large-top">
+
+<div>
+    <section class="tm-margin-large-top" style="padding-bottom: 1em">
         <div class="uk-container">
-
             <h1 class="uk-margin-medium-bottom uk-heading-hero">Select Products</h1>
-
             <hr class="uk-margin-remove">
             <div class="uk-grid-medium uk-child-width-1-3@m uk-grid-small uk-grid-match tm-margin-large-top uk-text-center"
-                data-uk-grid data-uk-scrollspy="cls: uk-animation-slide-bottom-medium; delay: 80">
-                <?php foreach ($data->products as $item) { ?>
-                <div>
-                    <a href="buy-product?id=<?= $item->id ?>&name=<?= $item->product_name ?>"
-                        class="uk-card uk-card-box uk-card-body uk-box-shadow-medium uk-border-rounded">
-                        <span class="uk-icon-forder uk-border-rounded"><span
+                data-uk-grid data-uk-scrollspy="cls: uk-animation-slide-bottom-medium; delay: 80"
+                style="overflow-y: scroll; z-index:200000">
+                <?php foreach ($data->products as $item) {
+                ?>
+                <div><a href="buy-product?id=<?= $item->id ?>&name=<?= $item->product_name ?>"
+                        class="uk-card uk-card-box uk-card-body uk-box-shadow-medium uk-border-rounded"><span
+                            class="uk-icon-forder uk-border-rounded"><span
                                 data-uk-icon="icon: pencil; ratio: 2"></span></span>
                         <p><?= $item->product_name ?></p>
-                    </a>
-                </div>
-                <?php } ?>
+                    </a></div><?php
+                                }
 
+                                    ?>
             </div>
-
-
         </div>
     </section>
-
-    <div class="uk-margin-large-top uk-section-primary">
-        <div class="uk-container">
-            <!-- <hr class="uk-margin-remove">
-        <div class="uk-grid-large uk-flex-middle uk-margin-xlarge-top uk-margin-xlarge-bottom" data-uk-grid data-uk-scrollspy="cls: uk-animation-slide-bottom; repeat: true">
-            <div class="uk-width-auto@m">
-                <p class="uk-text-large">Didn't find an answer to your question?</p>
-            </div>
-            <div class="uk-width-expand@m">
-                <a class="tm-button tm-button-xlarge" href="contact.html">Contact Us</a>
-            </div>
-        </div> -->
-        </div>
-    </div>
 </div>
-
-<footer class="uk-section tm-primary-color-dark uk-light uk-text-center">
-    <div class="uk-container">
-
-
-        <div class="uk-margin-medium tm-text-xsmall uk-text-meta copyright">Powered by <a
-                href="https://shrinqghana.com/">Shrinq</a></div>
-
-    </div>
-</footer>
-
-
-
-<div id="search" class="uk-flex-top uk-background-primary uk-modal-search" data-uk-modal>
-
-    <button class="uk-modal-close-full uk-close-large" type="button" data-uk-close></button>
-    <div class="uk-modal-dialog uk-text-center uk-margin-auto-vertical uk-padding-large">
-
-        <form class="uk-search uk-search-large uk-margin-auto">
-            <input class="uk-search-input uk-text-center" type="search" placeholder="Search...">
-        </form>
-
-    </div>
-</div>
-
-
-<div id="offcanvas" data-uk-offcanvas="flip: true; overlay: true">
-    <div class="uk-offcanvas-bar">
-
-        <button class="uk-offcanvas-close" type="button" data-uk-close></button>
-
-
-
-    </div>
-</div>
-
-
-</body>
-
-</html>
+<?php include('includes/footer.php') ?>
